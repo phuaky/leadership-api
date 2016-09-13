a19f5846ee7bf93ad76d71ed992a6dd0ce28f4f5
Irsyad <irsyad.muhd@gmail.com>
2016-09-13 14:37:36 +0800

    mycommit

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ app.listen(3000, function() {
   console.log('your server is up and running!');
 });
 
+app.set('view engine', 'ejs');
+
 var data = [
   {name: 'Irsyad', score: 100},
   {name: 'Kuan Yu', score: 5}
