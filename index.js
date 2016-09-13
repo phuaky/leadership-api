92839c31e061720fb40f51de4dc601401a5d07a7
Irsyad <irsyad.muhd@gmail.com>
2016-09-13 14:20:45 +0800

    mycommit

---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+
+var app = express()
+
+app.listen(3000, function() {
+  console.log('your server is up and running!');
+});
+
+var data = [
+  {name: 'Irsyad', score: 100},
+  {name: 'Kuan Yu', score: 5}
+]
+
+app.get("/", function(req, res) {
+  res.json(data);
+});
