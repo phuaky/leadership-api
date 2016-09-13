2abf7eed302d96f1958a3d9bcd1b712926d737e9
Phua Kuan Yu <Phuaky@virus.local>
2016-09-13 15:02:38 +0800

    main.js should be ok

--- static/main.js
+++ static/main.js
@@ -1,11 +1,23 @@
-// DOM manipulation here
 $(function () {
-  // your code here
-  console.log('DOM is working');
-
   $.ajax({
-    url: 'http://localhost:3000',
+    url: 'http://localhost:3000/data',
     type: 'GET'
   })
+    .done(function (data) {
+      console.log('success')
 
+      $('main').empty()
+      data.forEach(function (elem, index) {
+        $('main').append('<div class="board"  id="' + index + '">' +
+          '<div class="card-header">' + elem.name + '</div>' +
+          '<div class="card-block">' + elem.score + '</div>' +
+          '</div>')
+      })
+    })
+    .fail(function () {
+      console.log('error')
+    })
+    .always(function () {
+      console.log('complete')
+    })
 })
